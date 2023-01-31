ae57bbce179740e4dcb9a84eee8e1b1512850607
kwakdeung <rhkrwnsdud22@naver.com>
2023-01-31 09:59:45 +0900

    codeup_8

---
+++ codeup_100/codeup_100/src/codeup_100/codeup_8.java
@@ -0,0 +1,8 @@
+package codeup_100;
+
+public class codeup_8 {
+	
+	public static void main(String[] args) {
+		System.out.printf("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518");
+	}
+}
