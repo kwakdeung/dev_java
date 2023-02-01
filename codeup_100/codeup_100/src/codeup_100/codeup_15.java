deeb433d00a26b92e472e18295508b4282a44155
kwakdeung <rhkrwnsdud22@naver.com>
2023-02-01 09:22:55 +0900

    codeup_15

---
+++ codeup_100/codeup_100/src/codeup_100/codeup_15.java
@@ -0,0 +1,12 @@
+package codeup_100;
+
+import java.util.Scanner;
+
+public class codeup_15 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float a = sc.nextFloat();
+		System.out.printf("%.2f", a);
+	}
+}
