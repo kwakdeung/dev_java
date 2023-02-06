ce986f85b807f763a7ccaca469a7f7b6dc471071
kwakdeung <rhkrwnsdud22@naver.com>
2023-02-06 14:19:20 +0900

    codeup_24

---
+++ codeup_100/codeup_100/src/codeup_100/codeup_24.java
@@ -0,0 +1,18 @@
+package codeup_100;
+
+import java.util.Scanner;
+
+public class codeup_24 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		char[] arr = a.toCharArray();
+				
+				
+	for(int i=0;i<a.length();i++) {
+		System.out.println("'"+arr[i]+"'");
+	}
+		
+	}
+}
