45e5708c3979b8135649665698d422784b3c7b62
kwakdeung <rhkrwnsdud22@naver.com>
2023-02-10 09:33:49 +0900

    codeup_44

---
+++ codeup_100/codeup_100/src/codeup_100/codeup_44.java
@@ -0,0 +1,14 @@
+package codeup_100;
+
+import java.util.Scanner;
+
+public class codeup_44 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		
+		System.out.println(a+1);
+	}
+}
