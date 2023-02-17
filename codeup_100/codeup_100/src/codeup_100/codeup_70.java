c6cc2d6422578fa7b2da1c8f7256ee988a1a2940
kwakdeung <rhkrwnsdud22@naver.com>
2023-02-17 09:37:05 +0900

    codeup_70

---
+++ codeup_100/codeup_100/src/codeup_100/codeup_70.java
@@ -0,0 +1,39 @@
+package codeup_100;
+
+import java.util.Scanner;
+
+public class codeup_70 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+		switch(month) {
+		case 1:
+			System.out.println("Winter");break;
+		case 2:
+			System.out.println("Winter");break;
+		case 3:
+			System.out.println("Spring");break;
+		case 4:
+			System.out.println("Spring");break;
+		case 5:
+			System.out.println("Spring");break;
+		case 6:
+			System.out.println("Summer");break;
+		case 7:
+			System.out.println("Summer");break;
+		case 8:
+			System.out.println("Summer");break;
+		case 9:
+			System.out.println("Fall");break;
+		case 10:
+			System.out.println("Fall");break;
+		case 11:
+			System.out.println("Fall");break;
+		case 12:
+			System.out.println("Winter");break;
+		}
+
+	}
+
+}
