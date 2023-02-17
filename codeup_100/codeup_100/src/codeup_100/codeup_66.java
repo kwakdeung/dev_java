5889a449847e33c54e5d5fcb877715ff4af6d569
kwakdeung <rhkrwnsdud22@naver.com>
2023-02-17 09:36:43 +0900

    codeup_66

---
+++ codeup_100/codeup_100/src/codeup_100/codeup_66.java
@@ -0,0 +1,28 @@
+package codeup_100;
+
+import java.util.Scanner;
+
+public class codeup_66 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a%2==0) {
+			System.out.println("even");
+		} else {
+			System.out.println("odd");
+		}
+		if(b%2==0) {
+			System.out.println("even");
+		} else {
+			System.out.println("odd");
+		}
+		if(c%2==0) {
+			System.out.println("even");
+		} else {
+			System.out.println("odd");
+		}
+	}
+}
