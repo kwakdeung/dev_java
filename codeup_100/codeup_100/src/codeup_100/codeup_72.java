57c9bd8e7f2f68b3ac430b2ee7714cb8afe6ced8
kwakdeung <rhkrwnsdud22@naver.com>
2023-02-20 09:40:33 +0900

    codeup_72

---
+++ codeup_100/codeup_100/src/codeup_100/codeup_72.java
@@ -0,0 +1,17 @@
+package codeup_100;
+
+import java.util.Scanner;
+
+public class codeup_72 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			System.out.println(a);
+		}
+	}
+}
